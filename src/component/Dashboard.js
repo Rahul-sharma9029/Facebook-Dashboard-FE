7a2a8c2ebd937f53a0c5f7cd606b631f8db0e3ab
rohan694 <rohansharma996034@gmail.com>
2025-01-20 23:16:51 +0530

    sorting enhancements on dashboard

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { FaSortUp, FaSortDown } from 'react-icons/fa'; 
 import { fetchPages, fetchPageReels, fetchPageVideos } from "../services/api";
 import ReelPopup from "./ReelPopup";
 import AutomatePopup from "./AutomatePopup";
@@ -7,11 +8,12 @@ import "./Dashboard.css"; // Import CSS for styling
 const Dashboard = ({ accessToken }) => {
   const [isLoading, setIsLoading] = useState(false); // Loader state for page loading
   const [popupLoading, setPopupLoading] = useState(false); // Loader state for popup loading
-  // const [pages, setPages] = useState([]);
   const [pageDetails, setPageDetails] = useState([]);
   const [selectedPage, setSelectedPage] = useState(null); // State for the selected page to show reels/videos
   const [automationPage, setAutomationPage] = useState(null); // State for the page selected for automation
   const [error, setError] = useState(""); // Error state
+  const [sortBy, setSortBy] = useState(null); // State for sorting criteria (e.g., name, mostRecentViews, mostViewedViews)
+  const [sortOrder, setSortOrder] = useState("asc"); // State for sorting order (asc or desc)
 
   useEffect(() => {
     const fetchPageData = async () => {
@@ -19,7 +21,6 @@ const Dashboard = ({ accessToken }) => {
         setIsLoading(true);
         const data = await fetchPages(accessToken);
         const pages = data.pages || [];
-        // setPages(pages);
 
         const detailsPromises = pages.map((page) =>
           fetchPageReels(page.id, page.access_token)
@@ -71,9 +72,71 @@ const Dashboard = ({ accessToken }) => {
     return words.length > 4 ? words.slice(0, 4).join(" ") + "..." : title;
   };
 
+  const handleSort = (sortKey) => {
+    setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
+    setSortBy(sortKey);
+  };
+
+  const sortData = (data) => {
+    if (!sortBy) return data;
+
+    const sortOrderMultiplier = sortOrder === "asc" ? 1 : -1;
+
+    return data.slice().sort((a, b) => {
+      switch (sortBy) {
+        case "name":
+          return sortOrderMultiplier * (a.name.localeCompare(b.name));
+        case "mostRecentViews":
+          return (
+            sortOrderMultiplier *
+            (parseInt(a.details?.mostRecentReel?.views) -
+              parseInt(b.details?.mostRecentReel?.views) ||
+              (new Date(a.details?.mostRecentReel?.created_time) -
+                new Date(b.details?.mostRecentReel?.created_time)))
+          );
+        case "mostViewedViews":
+          return (
+            sortOrderMultiplier *
+            (parseInt(a.details?.mostViewedReel?.views) -
+              parseInt(b.details?.mostViewedReel?.views) ||
+              (new Date(a.details?.mostViewedReel?.created_time) -
+                new Date(b.details?.mostViewedReel?.created_time)))
+          );
+        case "mostRecentTimestamp":
+          return (
+            sortOrderMultiplier *
+            (new Date(a.details?.mostRecentReel?.created_time) -
+              new Date(b.details?.mostRecentReel?.created_time))
+          );
+        case "mostViewedTimestamp":
+          return (
+            sortOrderMultiplier *
+            (new Date(a.details?.mostViewedReel?.created_time) -
+              new Date(b.details?.mostViewedReel?.created_time))
+          );
+        default:
+          return data;
+      }
+    });
+  };
+
   return (
     <div className="dashboard">
       <h2>Facebook Pages Dashboard</h2>
+
+      <div className="sort-container">
+        <select
+          value={sortBy || ""}
+          onChange={(e) => handleSort(e.target.value)}
+        >
+          <option value="">Sort By</option>
+          <option value="name">Page Name</option>
+          <option value="mostRecentViews">Most Recent Video Views</option>
+          <option value="mostViewedViews">Most Viewed Video Views</option>
+          <option value="mostRecentTimestamp">Most Recent Video Timestamp</option>
+          <option value="mostViewedTimestamp">Most Viewed Video Timestamp</option>
+        </select>
+      </div>
 
       {!accessToken ? (
         <button
@@ -90,7 +153,11 @@ const Dashboard = ({ accessToken }) => {
               <tr>
                 <th>#</th>
                 <th>Page ID</th>
-                <th>Page Name</th>
+                <th onClick={() => handleSort("name")}>
+                  Page Name {sortBy === "name" && (
+                    <span className="sort-icon">{sortOrder === "asc" ? <FaSortUp /> : <FaSortDown />}</span>
+                  )}
+                </th>
                 {/* Merge column headers for "Most Recent Video" */}
                 <th colSpan="3" className="reel-column">
                   Most Recent Video
@@ -106,16 +173,44 @@ const Dashboard = ({ accessToken }) => {
                 <th></th>
                 <th></th>
                 <th className="reel-column">Reel Title</th>
-                <th className="reel-column">Views</th>
-                <th className="reel-column">Timestamp</th>
+                <th
+                  className="reel-column"
+                  onClick={() => handleSort("mostRecentViews")}
+                >
+                  Views {sortBy === "mostRecentViews" && (
+                   <span className="sort-icon">{sortOrder === "asc" ? <FaSortUp /> : <FaSortDown />}</span>
+                  )}
+                </th>
+                <th
+                  className="reel-column"
+                  onClick={() => handleSort("mostRecentTimestamp")}
+                >
+                  Timestamp {sortBy === "mostRecentTimestamp" && (
+                    <span className="sort-icon">{sortOrder === "asc" ? <FaSortUp /> : <FaSortDown />}</span>
+                  )}
+                </th>
                 <th className="reel-column">Reel Title</th>
-                <th className="reel-column">Views</th>
-                <th className="reel-column">Timestamp</th>
+                <th
+                  className="reel-column"
+                  onClick={() => handleSort("mostViewedViews")}
+                >
+                  Views {sortBy === "mostViewedViews" && (
+                    <span className="sort-icon">{sortOrder === "asc" ? <FaSortUp /> : <FaSortDown />}</span>
+                  )}
+                </th>
+                <th
+                  className="reel-column"
+                  onClick={() => handleSort("mostViewedTimestamp")}
+                >
+                  Timestamp {sortBy === "mostViewedTimestamp" && (
+                    <span className="sort-icon">{sortOrder === "asc" ? <FaSortUp /> : <FaSortDown />}</span>
+                  )}
+                </th>
                 <th></th>
               </tr>
             </thead>
             <tbody>
-              {pageDetails.map((page, index) => (
+              {sortData(pageDetails).map((page, index) => (
                 <React.Fragment key={page.id}>
                   <tr>
                     <td>{index + 1}</td>
@@ -145,10 +240,17 @@ const Dashboard = ({ accessToken }) => {
                     </td>
                     <td>
                       <div className="action-btn-container">
-                        <button className="action-btn" onClick={() => openPopup(page)} disabled={popupLoading}>
+                        <button
+                          className="action-btn"
+                          onClick={() => openPopup(page)}
+                          disabled={popupLoading}
+                        >
                           {popupLoading ? "Loading..." : "Show All Reels"}
                         </button>
-                        <button className="action-btn" onClick={() => openAutomationPopup(page)}>
+                        <button
+                          className="action-btn"
+                          onClick={() => openAutomationPopup(page)}
+                        >
                           Automate
                         </button>
                       </div>
